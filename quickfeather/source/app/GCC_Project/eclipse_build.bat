6082f67180bf7455aa6cd489fe9777f1797faf5f
rahmant3 <tamkinrahman94@gmail.com>
2021-03-27 10:30:08 -0600

    Clear app folder in order to restart with fpga uart project.

--- quickfeather/source/app/GCC_Project/eclipse_build.bat
+++
@@ -1,6 +0,0 @@
-:: Created to workaround build failures due to dependency files in Windows.
-
-set WORKSPACE=%~1
-
-call del /S /Q "%WORKSPACE%\..\output\depend\*.d"
-make -C "%WORKSPACE%/../" %2 %3 %4
