1200ebc8d7663b215e1794604d548a4b8a307111
gopalsih <41280664+gopalsih@users.noreply.github.com>
2021-04-03 14:41:25 -0500

    Slave SW TEST
    Added Slave SW for UART TEST

---
+++ slave_controller_software/Inc/pm_protocol.h
@@ -0,0 +1,155 @@
+#ifndef PM_PROTOCOL_H
+#define PM_PROTOCOL_H
+
+// --------------------------------------------------------------------------------------------------------------------
+// Repository:
+// - Github: https://github.com/rahmant3/polymath-iot
+//
+// Description:
+//   This file defines the core interface used to implement the polymath protocol. Example use:
+//
+/*
+  // Define driver functions
+  const pmProtocolDriver_t uart0Driver =
+  {
+    .tx = function name,
+    .rx = function name
+  };
+  // Declare RAM for the context.
+  pmProtocolContext_t uart0Context;
+
+  // Later...
+  if (PM_PROTOCOL_SUCCESS == pmProtocolInit(&uart0Driver, &uart0Context))
+  {
+    ...
+  }
+
+  */
+// --------------------------------------------------------------------------------------------------------------------
+
+// --------------------------------------------------------------------------------------------------------------------
+// INCLUDES
+// --------------------------------------------------------------------------------------------------------------------
+#include "pm_protocol_defs.h"
+#include <stdbool.h>
+
+// --------------------------------------------------------------------------------------------------------------------
+// DEFINES
+// --------------------------------------------------------------------------------------------------------------------
+#define PM_PROTOCOL_SUCCESS 0x00 //!< Function return code on success.
+#define PM_PROTOCOL_FAILURE 0x01 //!< Function return code on failure.
+
+#define PM_PROTOCOL_CHECKSUM_ERROR 0x02  //!< A packet read in progress failed due to a checksum error.
+#define PM_PROTOCOL_RX_TIMEOUT     0x03  //!< A packet read in progress failed due to a timeout.
+#define PM_PROTOCOL_TX_IN_PROGRESS 0x04  //!< A packet send failed due to another transmit in progress.
+
+#define PM_MAX_PAYLOAD_BYTES 64
+
+#define PM_NUM_OVERHEAD_BYTES 0x03
+#define MAX_TX_BYTES         (PM_MAX_PAYLOAD_BYTES + PM_NUM_OVERHEAD_BYTES)
+#define MAX_RX_BYTES         (PM_MAX_PAYLOAD_BYTES + PM_NUM_OVERHEAD_BYTES)
+
+// --------------------------------------------------------------------------------------------------------------------
+// TYPEDEFS
+// --------------------------------------------------------------------------------------------------------------------
+
+/**
+* @brief Driver function defined by the user, to transmit UART bytes.
+*
+* @param data The data bytes to send.
+* @param numBytes The number of bytes to send.
+* @return int The number of bytes sent.
+*/
+typedef int (*pmProtocolUartTxFcnPtr) (uint8_t * data, uint8_t numBytes);
+
+/**
+* @brief Driver function defined by the user, to receive UART bytes.
+*
+* @param buffer The data buffer to load with the read bytes.
+* @param numBytes Size of the data buffer.
+* @return int The number of bytes loaded to the buffer.
+*/
+typedef int (*pmProtocolUartRxFcnPtr) (uint8_t * buffer, uint8_t numBytes);
+
+
+struct pmProtocolDriver_s; // Forward declare
+/**
+* @brief Private context variables. The user must declare a structure in static RAM for this module to use. The module
+*        takes care of initializing the RAM.
+*
+*/
+typedef struct pmProtocolContext_s
+{
+    const struct pmProtocolDriver_s * driver;
+
+    bool txInProgress;
+    uint8_t txBuffer[MAX_TX_BYTES];
+
+    uint8_t rxState;
+    uint8_t rxLen;
+    uint8_t rxBytes;
+    uint32_t rxStartTicks;
+    uint8_t rxBuffer[MAX_RX_BYTES];
+} pmProtocolContext_t;
+
+/**
+* @brief Parameters that allow this module to interact with the lower level hardware.
+*
+*/
+typedef struct pmProtocolDriver_s
+{
+  pmProtocolUartTxFcnPtr tx; //!< Function used for transmitting data over UART.
+  pmProtocolUartRxFcnPtr rx; //!< Function used for receiving data over UART.
+} pmProtocolDriver_t;
+
+
+typedef struct pmProtocolRawPacket_s
+{
+  uint8_t bytes[PM_MAX_PAYLOAD_BYTES]; // The raw bytes to send.
+  uint8_t numBytes;  // The number of bytes in this->bytes.
+} pmProtocolRawPacket_t;
+
+// --------------------------------------------------------------------------------------------------------------------
+// FUNCTION PROTOTYPES
+// --------------------------------------------------------------------------------------------------------------------
+
+/**
+* @brief Call in order to initialize this module.
+*
+* @param driver Parameters that allow this module to interact with the hardware.
+* @param context Context variables that can be used by this module.
+* @return int #PM_PROTOCOL_SUCCESS on success, #PM_PROTOCOL_FAILURE otherwise.
+*/
+int pmProtocolInit(const pmProtocolDriver_t * driver, pmProtocolContext_t * context);
+
+/**
+* @brief This must be called periodically to allow this module to perform work.
+*
+* @param ticks_ms Current ticks in ms to allow this module to perform timeout detection.
+* @param context Context variables that can be used by this module.
+*/
+void pmProtocolPeriodic(uint32_t ticks_ms, pmProtocolContext_t * context);
+
+/**
+* @brief Call to queue up a message for transmission.
+*
+* @param tx The definition for the message to send.
+* @param context Context variables that can be used by this module.
+* @return int #PM_PROTOCOL_SUCCESS on success.
+*             #PM_PROTOCOL_TX_IN_PROGRESS if a transmit is already in progress.
+*             #PM_PROTOCOL_FAILURE otherwise.
+*/
+int pmProtocolSendPacket(pmProtocolRawPacket_t * tx, pmProtocolContext_t * context);
+
+/**
+* @brief Call to read a message from the receive queue.
+*
+* @param rx The buffer to load the received message into.
+* @param context Context variables that can be used by this module.
+* @return int #PM_PROTOCOL_SUCCESS if a message could be read.
+*             #PM_PROTOCOL_FAILURE if a message could not be read.
+*             #PM_PROTOCOL_TIMEOUT if a message was in progress but failed due to a timeout.
+*/
+int pmProtocolReadPacket(pmProtocolRawPacket_t * rx, pmProtocolContext_t * context);
+
+#endif // PM_PROTOCOL_H
---
+++ slave_controller_software/Src/pm_protocol.c
@@ -0,0 +1,228 @@
+// --------------------------------------------------------------------------------------------------------------------
+// Repository:
+// - Github: https://github.com/rahmant3/polymath-iot
+//
+// Description:
+//   This file implements the interface in pm_protocol.h.
+// --------------------------------------------------------------------------------------------------------------------
+
+// --------------------------------------------------------------------------------------------------------------------
+// INCLUDES
+// --------------------------------------------------------------------------------------------------------------------
+#include <stdbool.h>
+#include <string.h>
+
+#include "pm_protocol.h"
+
+// --------------------------------------------------------------------------------------------------------------------
+// DEFINES
+// --------------------------------------------------------------------------------------------------------------------
+#define START_BYTE   0x01
+#define END_OF_PACKET_TIMEOUT_ms 250
+
+// --------------------------------------------------------------------------------------------------------------------
+// TYPEDEFS
+// --------------------------------------------------------------------------------------------------------------------
+typedef enum pmProtocolRxStates_e
+{
+    WAITING_FOR_START,
+    WAITING_FOR_LEN,
+    WAITING_FOR_DATA,
+
+    PAYLOAD_READY,
+    TIMEOUT_OCCURRED,
+    CHECKSUM_ERROR
+} pmProtocolRxStates_t;
+
+// --------------------------------------------------------------------------------------------------------------------
+// VARIABLES
+// --------------------------------------------------------------------------------------------------------------------
+static pmProtocolContext_t g_pmProtocolContext;
+
+// --------------------------------------------------------------------------------------------------------------------
+// FUNCTIONS
+// --------------------------------------------------------------------------------------------------------------------
+int pmProtocolInit(const pmProtocolDriver_t * driver, pmProtocolContext_t * context)
+{
+    int rc = PM_PROTOCOL_FAILURE;
+    if (   (NULL != context)
+        && (NULL == context->driver) // If the driver is null, we can assume the module has not yet been initialized.
+        && (NULL != driver)
+        && (NULL != driver->rx)
+        && (NULL != driver->tx))
+    {
+        context->driver = driver;
+
+        (void)memset(&g_pmProtocolContext, 0, sizeof(g_pmProtocolContext));
+
+        rc = PM_PROTOCOL_SUCCESS;
+    }
+
+    return rc;
+}
+
+// --------------------------------------------------------------------------------------------------------------------
+int pmProtocolSendPacket(pmProtocolRawPacket_t * tx, pmProtocolContext_t * context)
+{
+    int rc = PM_PROTOCOL_FAILURE;
+    if ((NULL != context->driver) && (NULL != tx))
+    {
+        if (!context->txInProgress)
+        {
+            context->txInProgress = true;
+
+            uint8_t ix = 0;
+            context->txBuffer[0] = START_BYTE;
+            context->txBuffer[1] = tx->numBytes + PM_NUM_OVERHEAD_BYTES; // Extra 3 bytes for start, length, checksum.
+
+            (void)memcpy(&context->txBuffer[PM_NUM_OVERHEAD_BYTES - 1], tx->bytes, tx->numBytes);
+
+            uint8_t checksum = 0x00;
+            uint8_t len = context->txBuffer[1];
+            for (uint8_t index = 0; index < (len - 1); index++)
+            {
+                checksum += context->txBuffer[index];
+            }
+            context->txBuffer[ix++] = ~checksum;
+
+            rc = PM_PROTOCOL_SUCCESS;
+        }
+        else
+        {
+            rc = PM_PROTOCOL_TX_IN_PROGRESS;
+        }
+    }
+    return rc;
+}
+
+// --------------------------------------------------------------------------------------------------------------------
+int pmProtocolReadPacket(pmProtocolRawPacket_t * rx, pmProtocolContext_t * context)
+{
+    int rc = PM_PROTOCOL_FAILURE;
+
+    if ((NULL != context->driver) && (NULL != rx))
+    {
+        pmProtocolRxStates_t rxState = context->rxState;
+        if (rxState == TIMEOUT_OCCURRED)
+        {
+            rc = PM_PROTOCOL_RX_TIMEOUT;
+        }
+        else if (rxState == CHECKSUM_ERROR)
+        {
+            rc = PM_PROTOCOL_CHECKSUM_ERROR;
+        }
+        else if (rxState == PAYLOAD_READY)
+        {
+            rx->numBytes = context->rxBuffer[1] + PM_NUM_OVERHEAD_BYTES;
+            (void)memcpy(rx->bytes, &context->rxBuffer[PM_NUM_OVERHEAD_BYTES - 1], rx->numBytes);
+
+            rc = PM_PROTOCOL_SUCCESS;
+        }
+    }
+
+    return rc;
+}
+
+// --------------------------------------------------------------------------------------------------------------------
+void pmProtocolPeriodic(uint32_t ticks_ms, pmProtocolContext_t * context)
+{
+    if (NULL != context->driver)
+    {
+        // Handle transmits.
+        if (context->txInProgress)
+        {
+
+            if (0 < context->driver->tx(context->txBuffer, context->txBuffer[1]))
+            {
+                context->txInProgress = false; // Transmit completed.
+            }
+        }
+
+        // Handle receives.
+        switch(context->rxState)
+        {
+            case WAITING_FOR_START:
+            {
+                if (1 == context->driver->rx(context->rxBuffer, 1))
+                {
+                    if (context->rxBuffer[0] == START_BYTE)
+                    {
+                        context->rxStartTicks = ticks_ms;
+                        context->rxBytes = 1;
+                        context->rxState = WAITING_FOR_LEN;
+                    }
+                }
+                break;
+            }
+            case WAITING_FOR_LEN:
+            {
+                if (1 == context->driver->rx(&context->rxBuffer[1], 1))
+                {
+                    context->rxLen = context->rxBuffer[1];
+                    if (context->rxLen <= MAX_RX_BYTES)
+                    {
+                        context->rxBytes = 2;
+                        context->rxState = WAITING_FOR_DATA;
+                    }
+                    else
+                    {
+                        context->rxState = CHECKSUM_ERROR;
+                    }
+                }
+                else
+                {
+                    if ((context->rxStartTicks - ticks_ms) > END_OF_PACKET_TIMEOUT_ms)
+                    {
+                        // Timeout occurred.
+                        context->rxState = TIMEOUT_OCCURRED;
+                    }
+                }
+                break;
+            }
+            case WAITING_FOR_DATA:
+            {
+                uint8_t rxBytes = context->driver->rx(&context->rxBuffer[context->rxBytes],
+                    context->rxLen - context->rxBytes);
+
+                context->rxBytes += rxBytes;
+                if (context->rxBytes >= context->rxLen)
+                {
+                    // We have the full payload ready. Verify the checksum.
+                    uint8_t checksum = 0;
+                    for (uint8_t ix = 0; ix < context->rxLen; ix++)
+                    {
+                        checksum += context->rxBuffer[ix];
+                    }
+
+                    if (checksum == 0)
+                    {
+                        context->rxState = PAYLOAD_READY;
+                    }
+                    else
+                    {
+                        context->rxState = CHECKSUM_ERROR;
+                    }
+                }
+                else
+                {
+                    if ((context->rxStartTicks - ticks_ms) > END_OF_PACKET_TIMEOUT_ms)
+                    {
+                        // Timeout occurred.
+                        context->rxState = TIMEOUT_OCCURRED;
+                    }
+                }
+
+                break;
+            }
+            case PAYLOAD_READY:    // Pass through
+            case TIMEOUT_OCCURRED: // Pass through
+            case CHECKSUM_ERROR:   // Pass through
+            default:
+                break;
+        }
+    }
+}
+
+
+
+
